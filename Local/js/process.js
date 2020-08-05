86e01bc6abb1d6c414e73931134226d4725034b8
rpalakkal <roshan.palakkal@gmail.com>
2020-08-05 18:03:03 -0400

    Affectiva Emotion AI Summit + emoji from sdk

--- Local/js/process.js
+++ Local/js/process.js
@@ -1,11 +1,15 @@
 var JSSDK = JSSDK || {};
 JSSDK.Assets = {
-  "wasm": {
-      "affdex-native-bindings.wasm": "https://download.affectiva.com/js/wasm/affdex-native-bindings.wasm",
-      "affdex-native-bindings.js": "https://download.affectiva.com/js/wasm/affdex-native-bindings.js",
-      "affdex-native-bindings.data": "https://download.affectiva.com/js/wasm/affdex-native-bindings.data",
-      "affdex-worker.js": "https://download.affectiva.com/js/wasm/affdex-worker.js"
-  }
+  wasm: {
+    "affdex-native-bindings.wasm":
+      "https://download.affectiva.com/js/wasm/affdex-native-bindings.wasm",
+    "affdex-native-bindings.js":
+      "https://download.affectiva.com/js/wasm/affdex-native-bindings.js",
+    "affdex-native-bindings.data":
+      "https://download.affectiva.com/js/wasm/affdex-native-bindings.data",
+    "affdex-worker.js":
+      "https://download.affectiva.com/js/wasm/affdex-worker.js",
+  },
 };
 
 // SDK Needs to create video and canvas nodes in the DOM in order to function
@@ -21,7 +25,7 @@ var detector = new affdex.CameraDetector(divRoot, width, height, faceMode);
 detector.detectAllEmotions();
 detector.detectAllExpressions();
 detector.detectAllEmojis();
-detector.detectAllAppearance();  
+detector.detectAllAppearance();
 
 // //Add a callback to notify when the detector is initialized and ready for runing.
 // detector.addEventListener("onInitializeSuccess", function() {
@@ -32,68 +36,114 @@ detector.detectAllAppearance();
 // });
 
 function log(node_name, msg) {
-  console.log(msg)
+  console.log(msg);
 }
-onStart()
+onStart();
 //function executes when Start button is pushed.
 function onStart() {
   if (detector && !detector.isRunning) {
     detector.start(JSSDK.Assets.wasm);
   }
-  log('#logs', "Clicked the start button");
+  log("#logs", "Clicked the start button");
 }
 
 //function executes when the Stop button is pushed.
 function onStop() {
-  log('#logs', "Clicked the stop button");
+  log("#logs", "Clicked the stop button");
   if (detector && detector.isRunning) {
     detector.removeEventListener();
     detector.stop();
   }
-};
+}
 
 //function executes when the Reset button is pushed.
 function onReset() {
-  log('#logs', "Clicked the reset button");
+  log("#logs", "Clicked the reset button");
   if (detector && detector.isRunning) {
     detector.reset();
 
-    document.querySelector('#results').innerHTML = "";
+    document.querySelector("#results").innerHTML = "";
   }
-};
+}
 
 //Add a callback to notify when camera access is allowed
-detector.addEventListener("onWebcamConnectSuccess", function() {
-  log('#logs', "Webcam access allowed");
+detector.addEventListener("onWebcamConnectSuccess", function () {
+  log("#logs", "Webcam access allowed");
 });
 
 //Add a callback to notify when camera access is denied
-detector.addEventListener("onWebcamConnectFailure", function() {
-  log('#logs', "webcam denied");
+detector.addEventListener("onWebcamConnectFailure", function () {
+  log("#logs", "webcam denied");
   console.log("Webcam access denied");
 });
 
 //Add a callback to notify when detector is stopped
-detector.addEventListener("onStopSuccess", function() {
-  log('#logs', "The detector reports stopped");
+detector.addEventListener("onStopSuccess", function () {
+  log("#logs", "The detector reports stopped");
 });
 
+const mostFrequent = givenArray => {
+  let counts = {};
+  let maxValue = -1;
+  let maxItem = null;
+  for (const num of givenArray) {
+    if (!(num in counts)) {
+      counts[num] = 1;
+    } else {
+      counts[num] = counts[num] + 1;
+    }
+    if (counts[num] > maxValue) {
+      maxValue = counts[num];
+      maxItem = num;
+    }
+  }
+  return maxItem;
+};
+
+var emojiArray = [];
+var lastTimestamp = 0;
 //Add a callback to receive the results from processing an image.
 //The faces object contains the list of the faces detected in an image.
 //Faces object contains probabilities for all the different expressions, emotions and appearance metrics
-detector.addEventListener("onImageResultsSuccess", function(faces, image, timestamp) {
-  log('#results', "Timestamp: " + timestamp.toFixed(2));
-  log('#results', "Number of faces found: " + faces.length);
+detector.addEventListener("onImageResultsSuccess", function (
+  faces,
+  image,
+  timestamp
+) {
   if (faces.length > 0) {
-    log('#results', "Appearance: " + JSON.stringify(faces[0].appearance));
-    log('#results', "Emotions: " + JSON.stringify(faces[0].emotions, function(key, val) {
-      return val.toFixed ? Number(val.toFixed(0)) : val;
-    }));
-    log('#results', "Expressions: " + JSON.stringify(faces[0].expressions, function(key, val) {
-      return val.toFixed ? Number(val.toFixed(0)) : val;
-    }));
-    log('#results', "Emoji: " + faces[0].emojis.dominantEmoji);
-    console.log(faces)
+    emojiArray.push(faces[0].emojis.dominantEmoji);
+    // console.log(timestamp)
+    if (timestamp - lastTimestamp >= 2) {
+      lastTimestamp = timestamp;
+      // const datapoints = emojiArray.length;
+      // const summedEmojis = emojiArray.reduce((acc, emoji) => {
+      //   console.log(emoji)
+      //   Object.keys(emoji).map((attribute)=>{
+      //     if(!isNaN(emoji[attribute])){
+      //       acc[attribute] += emoji[attribute]
+      //     }
+      //   })
+      //   return acc
+      // }, {
+      //   disappointed: 0,
+      //   flushed: 0,
+      //   kissing: 0,
+      //   laughing: 0,
+      //   rage: 0,
+      //   relaxed: 0,
+      //   scream: 0,
+      //   smiley: 0,
+      //   smirk: 0,
+      //   stuckOutTongue: 0,
+      //   stuckOutTongueWinkingEye: 0,
+      //   wink: 0,
+      // });
+      // console.log(summedEmojis)
+
+      
+      $("#emotionDisplay").html(mostFrequent(emojiArray));
+      emojiArray = []
+    }
   }
   setTimeout(detector.captureNextImage, 150);
 });
